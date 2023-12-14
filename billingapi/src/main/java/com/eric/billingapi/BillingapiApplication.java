81d844af349905a47b42a3865a4beea712e4dffa
parameswari.bala <parameswaribala@gmail.com>
2023-12-14 15:06:02 +0530

    updated

---
+++ billingapi/src/main/java/com/eric/billingapi/BillingapiApplication.java
@@ -0,0 +1,13 @@
+package com.eric.billingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingapiApplication.class, args);
+	}
+
+}
---
+++ billingapi/src/test/java/com/eric/billingapi/BillingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.billingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
