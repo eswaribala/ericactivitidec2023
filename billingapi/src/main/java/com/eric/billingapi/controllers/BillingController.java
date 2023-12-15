16ff4059c58a30df428b0d4d94b45c9e67a03bbd
parameswari.bala <parameswaribala@gmail.com>
2023-12-15 10:15:55 +0530

    updated

--- billingapi/src/main/java/com/eric/billingapi/controllers/BillingController.java
+++ billingapi/src/main/java/com/eric/billingapi/controllers/BillingController.java
@@ -1,5 +1,10 @@
 package com.eric.billingapi.controllers;
 
+import org.activiti.api.task.runtime.TaskRuntime;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BillingController {
+	@Autowired
+	private RuntimeService runTimeService;
+	@Autowired
+	private TaskService taskService;
+	@Autowired
+	private TaskRuntime taskRunTime;
 	
 	@GetMapping("/start")
 	public ResponseEntity<?> startProcess(){
-		return ResponseEntity.status(HttpStatus.OK).body("Process Started......");
+	ProcessInstance processInstance=runTimeService.startProcessInstanceById("Process_qIZXOHi6h");	
+		
+		return ResponseEntity.status(HttpStatus.OK).body("Process Started......"+processInstance.getDeploymentId());
 	}
 
 }
